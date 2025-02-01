54541ecd867b379ead763beb71d488371e217b8d
Paul <paul@theapplegates.com>
2025-01-31 21:06:49 -0500

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,89 +1,94 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	plugins: [require("@tailwindcss/typography")],
-	theme: {
-		extend: {
-			typography: () => ({
-				DEFAULT: {
-					css: {
-						a: {
-							textUnderlineOffset: "2px",
-							"&:hover": {
-								"@media (hover: hover)": {
-									textDecorationColor: "var(--color-link)",
-									textDecorationThickness: "2px",
-								},
-							},
-						},
-						blockquote: {
-							borderLeftWidth: "0",
-						},
-						code: {
-							border: "1px dotted #666",
-							borderRadius: "2px",
-						},
-						kbd: {
-							"&:where([data-theme='dark'], [data-theme='dark'] *)": {
-								background: "var(--color-global-text)",
-							},
-						},
-						hr: {
-							borderTopStyle: "dashed",
-						},
-						strong: {
-							fontWeight: "700",
-						},
-						sup: {
-							marginInlineStart: "calc(var(--spacing) * 0.5)",
-							a: {
-								"&:after": {
-									content: "']'",
-								},
-								"&:before": {
-									content: "'['",
-								},
-								"&:hover": {
-									"@media (hover: hover)": {
-										color: "var(--color-link)",
-									},
-								},
-							},
-						},
-						/* Table */
-						"tbody tr": {
-							borderBottomWidth: "none",
-						},
-						tfoot: {
-							borderTop: "1px dashed #666",
-						},
-						thead: {
-							borderBottomWidth: "none",
-						},
-						"thead th": {
-							borderBottom: "1px dashed #666",
-							fontWeight: "700",
-						},
-						'th[align="center"], td[align="center"]': {
-							"text-align": "center",
-						},
-						'th[align="right"], td[align="right"]': {
-							"text-align": "right",
-						},
-						'th[align="left"], td[align="left"]': {
-							"text-align": "left",
-						},
-					},
-				},
-				sm: {
-					css: {
-						code: {
-							fontSize: "var(--text-sm)",
-							fontWeight: "400",
-						},
-					},
-				},
-			}),
-		},
-	},
+  plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "var(--font-base)",
+        serif: "var(--font-headings)",
+        mono: "var(--font-mono)",
+      },
+      typography: () => ({
+        DEFAULT: {
+          css: {
+            body: {
+              fontFamily: "var(--font-base)",
+            },
+            h1: { fontFamily: "var(--font-headings)" },
+            h2: { fontFamily: "var(--font-headings)" },
+            h3: { fontFamily: "var(--font-headings)" },
+            blockquote: {
+              borderLeftWidth: "0",
+              fontFamily: "var(--font-headings)",
+            },
+            code: {
+              border: "1px dotted #666",
+              borderRadius: "2px",
+              fontFamily: "var(--font-mono)",
+            },
+            kbd: {
+              "&:where([data-theme='dark'], [data-theme='dark'] *)": {
+                background: "var(--color-global-text)",
+              },
+            },
+            hr: {
+              borderTopStyle: "dashed",
+            },
+            strong: {
+              fontWeight: "700",
+            },
+            sup: {
+              marginInlineStart: "calc(var(--spacing) * 0.5)",
+              a: {
+                "&:after": {
+                  content: "']'",
+                },
+                "&:before": {
+                  content: "'['",
+                },
+                "&:hover": {
+                  "@media (hover: hover)": {
+                    color: "var(--color-link)",
+                  },
+                },
+              },
+            },
+            /* Table */
+            "tbody tr": {
+              borderBottomWidth: "none",
+            },
+            tfoot: {
+              borderTop: "1px dashed #666",
+            },
+            thead: {
+              borderBottomWidth: "none",
+            },
+            "thead th": {
+              borderBottom: "1px dashed #666",
+              fontWeight: "700",
+            },
+            'th[align="center"], td[align="center"]': {
+              "text-align": "center",
+            },
+            'th[align="right"], td[align="right"]': {
+              "text-align": "right",
+            },
+            'th[align="left"], td[align="left"]': {
+              "text-align": "left",
+            },
+          },
+        },
+        sm: {
+          css: {
+            code: {
+              fontSize: "var(--text-sm)",
+              fontWeight: "400",
+              fontFamily: "var(--font-mono)",
+            },
+          },
+        },
+      }),
+    },
+  },
 } satisfies Config;
