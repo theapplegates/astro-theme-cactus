453521bbe5bc6c5a8cfa29f765b9569addbcfc26
Paul <paul@theapplegates.com>
2025-03-05 15:40:51 -0500

    font

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 export default defineConfig({
 	site: siteConfig.url,
 	image: {
-		domains: ["webmention.io"],
+		domains: ["webmention.io", "res.cloudinary.com"],
 	},
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
