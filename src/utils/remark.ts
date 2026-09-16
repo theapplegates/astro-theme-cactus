c537e990d6ad7d455bf2cb60cf486805742f42ae
Chris Williams <chrismwilliams@users.noreply.github.com>
2026-09-16 15:56:11 +0100

    fix the type issue with mdast

--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -1,12 +1,12 @@
 import { h as _h, type Properties } from "hastscript";
-import type { Paragraph } from "mdast";
+import type { Paragraph as P } from "mdast";
+import type {} from "mdast-util-to-hast";
 
 /** From Astro Starlight: Function that generates an mdast HTML tree ready for conversion to HTML by rehype. */
-// biome-ignore lint/suspicious/noExplicitAny: allow any children
-export function h(el: string, attrs: Properties = {}, children: any[] = []): Paragraph {
-	const { properties, tagName } = _h(el, attrs);
+export function h(el: string, attrs: Properties = {}, children: unknown[] = []): P {
+	const { tagName, properties } = _h(el, attrs);
 	return {
-		children,
+		children: children as P['children'],
 		data: { hName: tagName, hProperties: properties },
 		type: "paragraph",
 	};
